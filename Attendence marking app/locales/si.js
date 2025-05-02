66030a556f858079fa387071ccf5b9c9606e39bc
IrushaChamalka <irushakavi@gmail.com>
2025-05-02 14:15:57 +0530

    language change

---
+++ Attendence marking app/locales/si.js
@@ -0,0 +1,18 @@
+export default {
+    welcome: "ආයුබෝවන් පරිශීලක",
+    markAttendance: "පැමිණීම සටහන් කරන්න",
+    selectLocation: "ස්ථානය තෝරන්න",
+    searchLocation: "ස්ථානය සොයන්න",
+    checkIn: "පැමිණීම සටහන් කරන්න",
+    checkOut: "නික්මීම සටහන් කරන්න",
+    logout: "පිටවීම",
+    login: "පිවිසෙන්න",
+    nicNumber: "ජා.හැ.අංකය",
+    password: "මුරපදය",
+    pleaseSelectLocation: "කරුණාකර පළමුව ස්ථානයක් තෝරන්න",
+    checkedInSuccess: "සාර්ථකව පැමිණීම සටහන් කරන ලදී!",
+    checkedOutSuccess: "සාර්ථකව නික්මීම සටහන් කරන ලදී!",
+    checkOutBeforeLogout: "පිටවීමට පෙර කරුණාකර නික්මීම සටහන් කරන්න",
+    loggedOut: "සාර්ථකව පිටවීම සිදුකරන ලදී",
+    invalidCredentials: "ජා.හැ.අංකය හෝ මුරපදය වලංගු නොවේ"
+  };
