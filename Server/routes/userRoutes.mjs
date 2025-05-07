7394071db378f7c6bb041ee2a06ec09055e965f7
chrishmika <shehanchrishmika123@gmail.com>
2025-05-07 15:26:27 +0530

    Add CI workflows for frontend and backend

--- Server/routes/userRoutes.mjs
+++ Server/routes/userRoutes.mjs
@@ -7,13 +7,12 @@ const userRoute = Router();
 
 // userRoute.use(requireAuth)
 
-//signIn
 userRoute.post("/signin", signInUser);
-//signOut
+
 userRoute.post("/signout", signOutUser);
-//signup create account
-userRoute.post("/signup", signupUser);
-//markAttendance
+
+userRoute.post("/signup", signupUser); // create new account
+
 userRoute.post("/attendanceSend", markAttendance); //done datavalidation is needed
 //data need to send [employID, , employName,date, workStatus,workspace, locationX, locationY];
 
