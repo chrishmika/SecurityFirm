7957f01bf449156ce69b39598101f091f8de20ac
chrishmika <shehanchrishmika123@gmail.com>
2025-06-26 04:10:47 +0530

    upgrade the code

--- Updated Project partial/Backend/lib/utils/createNotification.mjs
+++
@@ -1,9 +0,0 @@
-import Notification from "../../models/notification.model.mjs";
-
-const createNotification = async (to, from, fromModel = "User", type, description) => {
-  const newNotification = new Notification({ to, from, fromModel, type, description });
-  await newNotification.save();
-  console.log(newNotification);
-};
-
-export default createNotification;
