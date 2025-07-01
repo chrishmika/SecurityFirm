597edb54c61ff3c0198ef3d9873cf00e701fc02f
Asanka Kavinda <akavinda016@gmail.com>
2025-07-01 22:10:22 +0530

    update

--- Website-for-security-firm/src/Pages/JoinUs.jsx
+++ Website-for-security-firm/src/Pages/JoinUs.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import { NavLink, Outlet, Link } from "react-router-dom"
+import JobApplyForm from "../Components/Application Forms/JobApplyForm"
+
 
 const JoinUs = () => {
 
@@ -15,12 +17,9 @@ const JoinUs = () => {
           <h2 className="text-2xl text-blue-900 font-medium mb-4">Security Guard Application Process</h2>
           <p className="text-gray-600 mb-5">
              please fill out the form below and we will contact you shortly<br /><br />
-
             What Happens Next? <br />
             <ul className="list-disc  ml-5">
-              <li>Our HR team will review your application.
-
-</li>
+              <li>Our HR team will review your application.</li>
               <li>Shortlisted candidates will be contacted for an interview.</li>
               <li>Successful applicants undergo training (if required) before deployment</li>
             </ul>
@@ -28,18 +27,18 @@ const JoinUs = () => {
           
           <p className="text-gray-600 mb-5">Click the button below to start the application process on Indeed.</p>
         </div>
-
       </div>
-      <div>
+
+      {/* <div>
         <nav className="mb-7">
           <NavLink className={({ isActive }) => isActive ? 'bg-red-300 hover:bg-red-400 px-4 py-2 rounded-lg font-medium ' : 'bg-blue-400 hover:bg-blue-500 px-4 py-2 rounded-lg font-medium'} to='jobapply'>Apply for job</NavLink>
         </nav>
-        
-      </div>
-      <Outlet />
+      </div> */}
 
+      {/* <Outlet /> */}
 
-
+      <JobApplyForm/>
+      
     </div>
   )
 }
