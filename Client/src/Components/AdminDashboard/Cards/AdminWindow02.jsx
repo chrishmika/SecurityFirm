df391f1d8e67a95e2414e3147582352d99e9532a
Shehan Chrishmika <shehanchrishmika123@gmail.com>
2025-07-07 01:48:53 +0530

    readying for new release

--- Client/src/Components/AdminDashboard/Cards/AdminWindow02.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const AdminWindow02 = () => {
-  return <div className="bg-green-200 h-full rounded-2xl p-4">AdminWindow02</div>;
-};
-
-export default AdminWindow02;
--- Client/src/Pages/UserPages/User.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const User = () => {
-  return <div> i am a user mark attendance and view user detis</div>;
-};
-
-export default User;
