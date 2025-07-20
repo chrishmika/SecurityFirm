66bb91945b1a156b1f26e4a483b7c2b44140a2a9
Chrishmika <shehanchrishmika123@gmail.com>
2025-07-21 02:06:56 +0530

    markAttendance controller function #1

--- Backend/controllers/duty.controller.mjs
+++ Backend/controllers/duty.controller.mjs
@@ -55,6 +55,17 @@ export const deleteDutySheet = async (req, res) => {
   }
 };
 
+// need to complete this
+export const markAttendance = async (req, res) => {
+  try {
+    const { longitude, latitude, employeeId, time, dutyId } = req.body;
+
+    res.status(200).json({ message: `` });
+  } catch (error) {
+    console.log(`error in markAttendance ${error.message}`);
+    return res.status(500).json({ error: `internal server error on duty controller` });
+  }
+};
 // update Duty
 
 // view duty by company
