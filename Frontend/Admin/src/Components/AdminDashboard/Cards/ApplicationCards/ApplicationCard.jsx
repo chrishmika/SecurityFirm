9a8c857dec6b6b6dac2a8cf5b83c98361d755dea
Chrishmika <shehanchrishmika123@gmail.com>
2025-10-01 03:13:25 +0530

    popups for items

--- Frontend/Admin/src/Components/AdminDashboard/Cards/ApplicationCards/ApplicationCard.jsx
+++ Frontend/Admin/src/Components/AdminDashboard/Cards/ApplicationCards/ApplicationCard.jsx
@@ -24,6 +24,8 @@ import {
 } from "react-icons/md";
 import { RiProgress8Line } from "react-icons/ri";
 import { FaRegFileLines } from "react-icons/fa6";
+import { motion, AnimatePresence } from "motion/react";
+import { IoCloseSharp } from "react-icons/io5";
 
 const ApplicationCard = ({ data, choice }) => {
   console.log(data);
@@ -63,153 +65,166 @@ const ApplicationCard = ({ data, choice }) => {
         <span className=" cursor-pointer">{data?.name.split(" ", 1)}</span>
       </div>
 
-      <div
-        className={`${
-          isSelected ? "block" : "hidden"
-        } fixed inset-0 bg-black/30 backdrop-blur-sm z-50 flex items-center justify-center p-4`}>
-        <div className="relative w-full max-w-3xl bg-white rounded-lg p-6 md:p-10">
-          {/* Close Button */}
-          <span
-            className="absolute top-2 right-2 text-black hover:text-red-500 font-bold cursor-pointer"
-            onClick={() => setIsSelected(false)}>
-            ✕
-          </span>
+      <AnimatePresence>
+        {isSelected && (
+          <motion.div className="fixed inset-0 flex items-center justify-center backdrop-blur-xs bg-opacity-50 z-50 bg-[#0000007c]">
+            <motion.div
+              initial={{ opacity: 0, scale: 0 }}
+              animate={{ opacity: 1, scale: 1 }}
+              exit={{ opacity: 0, scale: 0 }}
+              transition={{ duration: 0.3 }}
+              className=" bg-blue-50 p-6 rounded-2xl shadow-2xl max-w-2xl h-auto w-full relative"
+              onClick={(e) => e.stopPropagation()}>
+              {/* Close Button */}
 
-          {/* Content: view employee application details */}
-          <div className="overflow-auto max-h-[80vh] flex flex-col gap-4">
-            {/* top layer */}
-            <div className="flex justify-between w-full">
-              <span className={`flex items-center gap-2 `}>
-                <FaClock />
-                <span className="font-bold">{"Created at : "}</span>
-                {data?.createdAt?.split("T")[0]}
-              </span>
+              <div className="flex justify-end z-100 relative bottom-5 left-fit -top-3 ">
+                <IoCloseSharp
+                  className=" text-white text-2xl bg-gray-800 rounded-full flex cursor-pointer hover:bg-red-400"
+                  onClick={() => setIsSelected(false)}
+                />
+              </div>
 
-              <span
-                className={`flex items-center gap-4 ${
-                  data?.status == "waiting"
-                    ? "text-blue-500"
-                    : data?.status == "reject"
-                    ? "text-red-500"
-                    : "text-green-500"
-                }
+              {/* Content: view employee application details */}
+              <div className="overflow-auto max-h-[80vh] flex flex-col gap-4">
+                {/* top layer */}
+                <div className="flex justify-between w-full">
+                  <span className={`flex items-center gap-2 `}>
+                    <FaClock />
+                    <span className="font-bold">{"Created at : "}</span>
+                    {data?.createdAt?.split("T")[0]}
+                  </span>
+
+                  <span
+                    className={`flex items-center gap-4 ${
+                      data?.status == "waiting"
+                        ? "text-blue-500"
+                        : data?.status == "reject"
+                        ? "text-red-500"
+                        : "text-green-500"
+                    }
               `}>
-                <RiProgress8Line />
-                {data?.status}
-              </span>
-            </div>
-
-            {/* middle layer */}
-            <span className="flex items-center gap-4 text-2xl font-bold mb-3">
-              {!choice && <FaUser />}
-              {choice && <FaBuilding />}
-              {data?.name}
-            </span>
-
-            <div className="mx-5">
-              <span className="flex items-center gap-4">
-                <MdEmail />
-                {data?.email}
-              </span>
-
-              <span className="flex items-center gap-4">
-                <FaHome />
-                {data?.address}
-              </span>
-
-              <span className="flex items-center gap-4">
-                <FaPhoneAlt />
-                {data?.contact}
-              </span>
-
-              <span className={`flex items-center gap-4 ${choice ? "hidden" : ""}`}>
-                <FaBirthdayCake />
-                {data?.dob}
-              </span>
-
-              <span className={`flex items-center gap-4 ${choice ? "hidden" : ""}`}>
-                <BiMaleFemale />
-                {data?.sex}
-              </span>
-
-              {/* conpany only data is need to be added */}
-              {choice && (
-                <div>
-                  <span className={`flex items-center gap-4 ${choice ? "" : "hidden"}`}>
-                    <MdDescription />
-                    {data?.description}
-                  </span>
-
-                  <span className={`flex items-center gap-4 ${choice ? "" : "hidden"}`}>
-                    <MdOutlineDateRange />
-                    {data?.date}
-                  </span>
-
-                  <span className={`flex items-center gap-4 ${choice ? "" : "hidden"}`}>
-                    <MdDateRange />
-                    {`${data?.period || "Contract "} days`}
-                  </span>
-
-                  <span className={`flex items-center gap-4 ${choice ? "" : "hidden"}`}>
-                    <MdOutlineDataset />
-                    {data?.type}
+                    <RiProgress8Line />
+                    {data?.status}
                   </span>
                 </div>
-              )}
 
-              <div className={`flex my-8 gap-5 ${choice ? "hidden" : ""}`}>
-                <a
-                  href={data?.cv ? data?.cv.replace("/upload/", "/upload/fl_attachment/") : "#"}
-                  download={"cv"}
-                  className="flex flex-col items-center text-blue-600 hover:underline"
-                  target="_blank"
-                  rel="noopener noreferrer">
-                  {isImage ? (
-                    <img src={data.cv} alt={"cv"} className="h-20 w-20 object-cover rounded mb-2" />
-                  ) : isPdf ? (
-                    <div className="h-20 w-20 flex items-center justify-center bg-red-100 text-red-600 font-bold rounded mb-2">
-                      PDF
-                    </div>
-                  ) : (
-                    <div className="h-20 w-20 flex items-center justify-center bg-gray-200 text-gray-600 font-bold rounded mb-2">
-                      FILE
+                {/* middle layer */}
+                <span className="flex items-center gap-4 text-2xl font-bold mb-3">
+                  {!choice && <FaUser />}
+                  {choice && <FaBuilding />}
+                  {data?.name}
+                </span>
+
+                <div className="mx-5">
+                  <span className="flex items-center gap-4">
+                    <MdEmail />
+                    {data?.email}
+                  </span>
+
+                  <span className="flex items-center gap-4">
+                    <FaHome />
+                    {data?.address}
+                  </span>
+
+                  <span className="flex items-center gap-4">
+                    <FaPhoneAlt />
+                    {data?.contact}
+                  </span>
+
+                  <span className={`flex items-center gap-4 ${choice ? "hidden" : ""}`}>
+                    <FaBirthdayCake />
+                    {data?.dob}
+                  </span>
+
+                  <span className={`flex items-center gap-4 ${choice ? "hidden" : ""}`}>
+                    <BiMaleFemale />
+                    {data?.sex}
+                  </span>
+
+                  {/* conpany only data is need to be added */}
+                  {choice && (
+                    <div>
+                      <span className={`flex items-center gap-4 ${choice ? "" : "hidden"}`}>
+                        <MdDescription />
+                        {data?.description}
+                      </span>
+
+                      <span className={`flex items-center gap-4 ${choice ? "" : "hidden"}`}>
+                        <MdOutlineDateRange />
+                        {data?.date}
+                      </span>
+
+                      <span className={`flex items-center gap-4 ${choice ? "" : "hidden"}`}>
+                        <MdDateRange />
+                        {`${data?.period || "Contract "} days`}
+                      </span>
+
+                      <span className={`flex items-center gap-4 ${choice ? "" : "hidden"}`}>
+                        <MdOutlineDataset />
+                        {data?.type}
+                      </span>
                     </div>
                   )}
-                  {"CV"}
-                </a>
 
-                <a
-                  href={
-                    data?.NICCopy
-                      ? data?.NICCopy.replace("/upload/", "/upload/fl_attachment/")
-                      : "#"
-                  }
-                  download={"NIC"}
-                  className="flex flex-col items-center text-blue-600 hover:underline"
-                  target="_blank"
-                  rel="noopener noreferrer">
-                  {isImagei ? (
-                    <img
-                      src={data.NICCopy}
-                      alt={"NICCopy"}
-                      className="h-20 w-20 object-cover rounded mb-2"
-                    />
-                  ) : isPdfi ? (
-                    <div className="h-20 w-20 flex items-center justify-center bg-red-100 text-red-600 font-bold rounded mb-2">
-                      File
-                    </div>
-                  ) : (
-                    <div className="h-20 w-20 flex items-center justify-center bg-gray-200 text-gray-600 font-bold rounded mb-2">
-                      FILE
-                    </div>
-                  )}
-                  {"NICCopy"}
-                </a>
+                  <div className={`flex my-8 gap-5 ${choice ? "hidden" : ""}`}>
+                    <a
+                      href={data?.cv ? data?.cv.replace("/upload/", "/upload/fl_attachment/") : "#"}
+                      download={"cv"}
+                      className="flex flex-col items-center text-blue-600 hover:underline"
+                      target="_blank"
+                      rel="noopener noreferrer">
+                      {isImage ? (
+                        <img
+                          src={data.cv}
+                          alt={"cv"}
+                          className="h-20 w-20 object-cover rounded mb-2"
+                        />
+                      ) : isPdf ? (
+                        <div className="h-20 w-20 flex items-center justify-center bg-red-100 text-red-600 font-bold rounded mb-2">
+                          PDF
+                        </div>
+                      ) : (
+                        <div className="h-20 w-20 flex items-center justify-center bg-gray-200 text-gray-600 font-bold rounded mb-2">
+                          FILE
+                        </div>
+                      )}
+                      {"CV"}
+                    </a>
+
+                    <a
+                      href={
+                        data?.NICCopy
+                          ? data?.NICCopy.replace("/upload/", "/upload/fl_attachment/")
+                          : "#"
+                      }
+                      download={"NIC"}
+                      className="flex flex-col items-center text-blue-600 hover:underline"
+                      target="_blank"
+                      rel="noopener noreferrer">
+                      {isImagei ? (
+                        <img
+                          src={data.NICCopy}
+                          alt={"NICCopy"}
+                          className="h-20 w-20 object-cover rounded mb-2"
+                        />
+                      ) : isPdfi ? (
+                        <div className="h-20 w-20 flex items-center justify-center bg-red-100 text-red-600 font-bold rounded mb-2">
+                          File
+                        </div>
+                      ) : (
+                        <div className="h-20 w-20 flex items-center justify-center bg-gray-200 text-gray-600 font-bold rounded mb-2">
+                          FILE
+                        </div>
+                      )}
+                      {"NICCopy"}
+                    </a>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
-        </div>
-      </div>
+            </motion.div>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 };
--- Frontend/Admin/src/Components/AdminDashboard/Cards/NotificationCards/NotificationCard.jsx
+++ Frontend/Admin/src/Components/AdminDashboard/Cards/NotificationCards/NotificationCard.jsx
@@ -4,6 +4,7 @@ import { FaRegStar } from "react-icons/fa";
 import { FaStar } from "react-icons/fa";
 import { IoCloseSharp } from "react-icons/io5";
 import axios from "axios";
+import { motion, AnimatePresence } from "motion/react";
 
 const NotificationCard = ({ notification }) => {
   let { to, fromModel, type, favourite, read, description, createdAt, _id, from } = notification;
@@ -57,12 +58,14 @@ const NotificationCard = ({ notification }) => {
           ) : (
             <FaRegStar className=" m-1.5 text-2xl" onClick={favBtnHandler} />
           )}
+
           <div className="flex md:flex-row flex-col gap-2 ">
             <button
               onClick={viewNotificationhandler}
               className="bg-green-500 rounded-2xl px-4 py-2 text-white cursor-pointer">
               Read
             </button>
+
             <button
               onClick={deleteNotificationHandler}
               className="bg-red-500 rounded-2xl px-4 py-2 text-white cursor-pointer">
@@ -72,51 +75,59 @@ const NotificationCard = ({ notification }) => {
         </div>
       </div>
 
-      <div
-        className={`${
-          viewNotification ? "box" : "hidden"
-        } absolute inset-0 backdrop-blur-sm p-10 z-100 `}>
-        <div>
-          <div className=" flex absolute md:inset-20 sm:inset-y-50 inset-y-20 inset-x-10 inset bg-red-400 ml-15 sm:mx-50">
-            <div className="flex justify-end z-999 relative bottom-5 left-full ">
-              <IoCloseSharp
-                className=" text-white text-2xl bg-gray-800 rounded-full flex cursor-pointer  hover:bg-red-400"
-                onClick={viewNotificationhandler}
-              />
-            </div>
-            {/*data on notification */}
-            <div>
-              <div className="flex flex-col justify-center m-5 w-md relative">
-                <span className="text-2xl font-bold">From :{from?.name}</span>
-                <span className="text-sm ">NIC :{from?.NIC}</span>
-
-                <span className="text-gray-900 text-sm">
-                  Created At : {createdAt?.split("T")[0]}
-                </span>
-
-                <div className="pb-7">
-                  <span>Message : {description}</span>
-                </div>
-
-                <div className="flex md:flex-row flex-col gap-2 ">
-                  {favourites ? (
-                    <FaStar className="cursor-pointer m-1.5 text-2xl" onClick={favBtnHandler} />
-                  ) : (
-                    <FaRegStar className=" m-1.5 text-2xl cursor-pointer" onClick={favBtnHandler} />
-                  )}
-
-                  <button
-                    onClick={deleteNotificationHandler}
-                    className="bg-red-500 rounded-2xl px-4 py-2 text-white cursor-pointer">
-                    Delete
-                  </button>
-                </div>
-                <span></span>
+      <AnimatePresence>
+        {viewNotification && (
+          <motion.div className="fixed inset-0 flex items-center justify-center backdrop-blur-xs h-screen bg-opacity-50 z-50 bg-[#0000007c]">
+            <motion.div
+              initial={{ opacity: 0, scale: 0 }}
+              animate={{ opacity: 1, scale: 1 }}
+              exit={{ opacity: 0, scale: 0 }}
+              transition={{ duration: 0.3 }}
+              className=" bg-blue-50 p-6 rounded-2xl shadow-2xl max-w-2xl h-fit w-fit relative"
+              onClick={(e) => e.stopPropagation()}>
+              <div className="flex justify-end z-100 relative bottom-5 left-fit -top-1 ">
+                <IoCloseSharp
+                  className=" text-white text-2xl bg-gray-800 rounded-full flex cursor-pointer hover:bg-red-400"
+                  onClick={viewNotificationhandler}
+                />
               </div>
-            </div>
-          </div>
-        </div>
-      </div>
+              {/*data on notification */}
+              <div>
+                <div className="flex flex-col justify-center m-5 w-md relative">
+                  <span className="text-2xl font-bold">From :{from?.name}</span>
+                  <span className="text-sm ">NIC :{from?.NIC}</span>
+
+                  <span className="text-gray-900 text-sm">
+                    Created At : {createdAt?.split("T")[0]}
+                  </span>
+
+                  <div className="pb-7">
+                    <span>Message : {description}</span>
+                  </div>
+
+                  <div className="flex md:flex-row flex-col gap-2 ">
+                    {favourites ? (
+                      <FaStar className="cursor-pointer m-1.5 text-2xl" onClick={favBtnHandler} />
+                    ) : (
+                      <FaRegStar
+                        className=" m-1.5 text-2xl cursor-pointer"
+                        onClick={favBtnHandler}
+                      />
+                    )}
+
+                    <button
+                      onClick={deleteNotificationHandler}
+                      className="bg-red-500 rounded-2xl px-4 py-2 text-white cursor-pointer">
+                      Delete
+                    </button>
+                  </div>
+                  <span></span>
+                </div>
+              </div>
+            </motion.div>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 };
