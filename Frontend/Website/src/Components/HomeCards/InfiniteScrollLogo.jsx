6a26de2e15ea4439e149dffb84d344d0cfb86778
Chrishmika <shehanchrishmika123@gmail.com>
2025-09-30 14:48:44 +0530

    web site client logo updated

--- Frontend/Website/src/Components/HomeCards/InfiniteScrollLogo.jsx
+++ Frontend/Website/src/Components/HomeCards/InfiniteScrollLogo.jsx
@@ -1,35 +1,32 @@
-import { clients } from '../../AdditionalData.json'
+import { clients } from "../../AdditionalData.json";
 
 const InfiniteLogoScrall = () => {
-    return (
-        <div>
-            {/* <div>companies</div> */}
-
-            <div className=" flex w-full overflow-hidden bg-white py-8">
-            <div className="relative">
-                <div className="flex animate-scroll-left">
-                    {/* first */}
-                    {clients.map((client, index) => (
-                        <div key={`first-${index}`} className="flex-shrink-0 mx-4 md:mx-8" area-hidden="true">
-
-                            <img className='w-50 h-auto' src={client.logo} alt={client.alt} />
-                        </div>
-                    ))}
-                    {/* second */}
-                    {clients.map((client, index) => (
-                        <div key={`second-${index}`} className="flex-shrink-0 mx-4 md:mx-8">
-
-                            <img className='w-50 h-auto' src={client.logo} alt={client.alt} />
-                        </div>
-                    ))}
-                </div>
-            </div>
-
+  return (
+    <div>
+      {/* <div>companies</div> */}
+      <div className=" flex w-full overflow-hidden bg-white py-8">
+        <div className="relative">
+          <div className="flex animate-scroll-left justify-center items-center">
+            {/* first */}
+            {clients.map((client, index) => (
+              <div
+                key={`first-${index}`}
+                className=" flex-shrink-0 mx-4 md:mx-8"
+                aria-hidden="true">
+                <img className="w-52 h-auto " src={`${client.logo}`} alt={client.alt} />
+              </div>
+            ))}
+            {/* second */}
+            {clients.map((client, index) => (
+              <div key={`second-${index}`} className="flex-shrink-0 mx-4 md:mx-8 ">
+                <img className="w-50 h-auto" src={client.logo} alt={client.alt} />
+              </div>
+            ))}
+          </div>
         </div>
+      </div>
+    </div>
+  );
+};
 
-        </div>
-        
-    )
-}
-
-export default InfiniteLogoScrall
+export default InfiniteLogoScrall;
