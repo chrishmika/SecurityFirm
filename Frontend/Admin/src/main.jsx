2e541eaa6a96677a1b3f4bc397f8212f61df4ad0
Chrishmika <shehanchrishmika123@gmail.com>
2025-11-19 01:01:37 +0530

    Configure admin

--- Frontend/Admin/src/main.jsx
+++ Frontend/Admin/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
+// import { BrowserRouter } from "react-router-dom";
 import { AuthContexProvider } from "./context/AuthContextProvider.jsx";
 import { EmployeeProvider } from "./context/EmployeeContextProvider.jsx";
 import { CompanyProvider } from "./context/CompanyContextProvider.jsx";
--- Frontend/Admin/vite.config.js
+++ Frontend/Admin/vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/app/",
+  // base: "/app/",
   server: {
     port: 2000,
     proxy: {
