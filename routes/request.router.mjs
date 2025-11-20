53bd176cae36b3c2cd2c3df7fffcf2ec9fbe9beb
Irusha <135311090+IrushaChamalka@users.noreply.github.com>
2025-11-20 10:57:29 +0530

    Uncomment and enable company and employee request routes

--- routes/request.router.mjs
+++ routes/request.router.mjs
@@ -19,20 +19,20 @@ import { adminAccess } from "../middleware/adminAccess.mjs";
 const router = express.Router();
 
 //only admins can access here
-router.use(adminAccess); //works
+router.use(adminAccess); 
 
-router.post("/companyReq", protectedRoute, getCompanyRequests); //not tested
-router.post("/companyReq/:id", protectedRoute, viewCompanyRequest); //not tested
-router.put("/favCompanyReq/:id", protectedRoute, markFavouriteCompanyReqFavourite); //not tested
+router.post("/companyReq", protectedRoute, getCompanyRequests); 
+router.post("/companyReq/:id", protectedRoute, viewCompanyRequest); 
+router.put("/favCompanyReq/:id", protectedRoute, markFavouriteCompanyReqFavourite); 
 router.delete("/companyDel/:id", protectedRoute, deleteCompanyRequest);
-router.post("/favCompanyReq", protectedRoute, favouriteCompanyRequests); //not tested
-router.post("/companyReq/:id", protectedRoute, acceptCompanyReq); //not tested
+router.post("/favCompanyReq", protectedRoute, favouriteCompanyRequests); 
+router.post("/companyReq/:id", protectedRoute, acceptCompanyReq); 
 
-router.post("/employeeReq", protectedRoute, getEmployeeRequests); //not tested
-router.post("/employeeReq/:id", protectedRoute, viewEmployeeRequest); //not tested
-router.put("/favEmployeeReq/:id", protectedRoute, markEmployeeteReqFavourite); //not tested
+router.post("/employeeReq", protectedRoute, getEmployeeRequests); 
+router.post("/employeeReq/:id", protectedRoute, viewEmployeeRequest); 
+router.put("/favEmployeeReq/:id", protectedRoute, markEmployeeteReqFavourite); 
 router.delete("/employeeDel/:id", protectedRoute, deleteEmployeeRequest);
-router.post("/favEmployeeReq", protectedRoute, favouriteEmployeeRequests); //not tested
-router.post("/employeeReq/:id", protectedRoute, acceptEmployeeReq); //not tested
+router.post("/favEmployeeReq", protectedRoute, favouriteEmployeeRequests); 
+router.post("/employeeReq/:id", protectedRoute, acceptEmployeeReq); 
 
 export default router;
